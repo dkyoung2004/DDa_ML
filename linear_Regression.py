6cf97c576d5d499c3de41b47e154e7e9b254d66b
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2020-11-14 17:21:58 +0900

    ML
    선형회귀 완료,그러나 로지스틱 코스트 함수 미분 진행중

--- linear_Regression.py
+++ linear_Regression.py
@@ -7,20 +7,20 @@ x = np.array(x)
 y = np.array(y)
 #초기 기울기와 y절편을 랜덤으로 설정
 #rmse방식으로 할 수도 있었으나 우선은 이렇게 설정.
-W = np.random.random()
-b =np.random.random()
+W = np.random.rand()
+b =np.random.rand()
 
 def decent(x,y,W,b,lr):
     #일차함수에서 비롯된 y의 값과 본 데이터셋의 y값의 차이를 제곱하고, 데이터의 개수만큼 나눠주는데, 미분을 하면
     #지수가 계수로 가는 것을 인지하고 2로도 한번 더 나눠줌
-    cost = np.sum((W*x+b-y)**2)/(len(x))
+    cost = np.sum((W*x+b-y)**2)/len(x)
     gradient_W =np.sum((W*x+b-y)*2*x)/len(x) #위에 코스트 함수를 미분한 것
     gradient_b =np.sum((W*x+b-y)*2)/len(x) #y값의 오차 만 봄 어차피 더하기 빼기만 하면되니께
     W -= lr*gradient_W
     b -= lr*gradient_b
     return cost,W
 lr =0.002
-for i in range (1,600):
+for i in range (1,300):
     cost,W = decent(x,y,W,b,lr)
     print("cost:",cost,"W",W)
 
