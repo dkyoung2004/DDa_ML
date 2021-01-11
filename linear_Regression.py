ce095898559e95e9d6bceb99e42340c5985eaaac
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2021-01-11 21:15:16 +0900

    Update linear_Regression.py

--- linear_Regression.py
+++ linear_Regression.py
@@ -12,12 +12,8 @@ x = np.array(x)
 y = np.array(y)
 #초기 기울기와 y절편을 랜덤으로 설정
 #rmse방식으로 할 수도 있었으나 우선은 이렇게 설정.
-figure,ax = plt.subplots(figsize=(4,3))
-plt.ion()
-
 W = np.random.rand()
 b =np.random.rand()
-line, = ax.plot(x,y) 
 def decent(x,y,W,b,lr):
     #일차함수에서 비롯된 y의 값과 본 데이터셋의 y값의 차이를 제곱하고, 데이터의 개수만큼 나눠주는데, 미분을 하면
     #지수가 계수로 가는 것을 인지하고 2로도 한번 더 나눠줌
@@ -26,20 +22,15 @@ def decent(x,y,W,b,lr):
     gradient_b =np.sum((W*x+b-y)*2)/len(x) #y값의 오차 만 봄 어차피 더하기 빼기만 하면되니께
     W -= lr*gradient_W
     b -= lr*gradient_b
-    return W,b
-lr =0.002
+    return cost,W
+lr =0.001
 
-for p in range(200):
-    W1,b1=decent(x,y,W,b,lr)
-    line.set_xdata(x)
-    line.set_ydata(W1*x+b1)
-    figure.canvas.draw()
-
-    figure.canvas.flush_events()
-    time.sleep(0.1)
-for i in range(1,400):
-    W,b =decent(X,Y,W,b,lr)
-    print("W:",W,"b:",b)
+for i in range(1,800):
+    cost,W =decent(x,y,W,b,lr)
+    print("W:",W,"cost:",cost)
+plt.plot(x,y)
+plt.plot(x,(W*x+b),color="red")
+plt.show()
 """
 def animate(x,y,W,b,lr):
     cost,W,b = decent(x,y,W,b,lr)
@@ -59,5 +50,13 @@ plt.show()
 for i in range (1,300):
     cost,W = decent(x,y,W,b,lr)
     print("cost:",cost,"W",W)
+for p in range(200):
+    W1,b1=decent(x,y,W,b,lr)
+    line.set_xdata(x)
+    line.set_ydata(W1*x+b1)
+    figure.canvas.draw()
+
+    figure.canvas.flush_events()
+    time.sleep(0.1)
 """
 
